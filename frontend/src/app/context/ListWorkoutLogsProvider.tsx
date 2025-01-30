88f0e4024af53386f01d1f664427dbd07ce279a6
Simon Serrano <simon.serrano@busit.com>
2025-01-30 11:46:35 +0100

    style(frontend): fixed max line length rule

--- frontend/src/app/context/ListWorkoutLogsProvider.tsx
+++ frontend/src/app/context/ListWorkoutLogsProvider.tsx
@@ -2,7 +2,8 @@ import { PropsWithChildren } from 'react';
 import { ListWorkoutLogsContext } from './ListWorkoutLogsContext';
 import ListWorkoutLogsUseCase from '../usecase/ListWorkoutLogsUseCase';
 import WorkoutLogClient from '../../adapter/out/WorkoutLogClient';
-import WorkoutLogResponseMapper from '../port/out/mapper/WorkoutLogResponseMapper';
+import WorkoutLogResponseMapper 
+  from '../port/out/mapper/WorkoutLogResponseMapper';
 
 export default function ListWorkoutLogsProvider(props: PropsWithChildren) {
   const useCase = new ListWorkoutLogsUseCase(
