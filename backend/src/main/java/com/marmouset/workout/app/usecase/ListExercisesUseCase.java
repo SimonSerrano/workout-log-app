ba4f310d1f0d1ac3b32f68d0296ba0555b2b058c
Simon Serrano <simon.serrano@busit.com>
2025-01-30 20:41:18 +0100

    style(backend): format code

--- backend/src/main/java/com/marmouset/workout/app/usecase/ListExercisesUseCase.java
+++ backend/src/main/java/com/marmouset/workout/app/usecase/ListExercisesUseCase.java
@@ -1,29 +1,39 @@
 package com.marmouset.workout.app.usecase;
 
-import java.util.stream.StreamSupport;
-
-import org.springframework.stereotype.Component;
-
 import com.marmouset.workout.app.port.in.ListExercises;
 import com.marmouset.workout.app.port.out.ExercisePresenter;
 import com.marmouset.workout.app.port.out.ExerciseRepository;
 import com.marmouset.workout.app.port.out.dto.ExerciseResponse;
+import java.util.stream.StreamSupport;
+import org.springframework.stereotype.Component;
 
+/**
+ * Use case to list all exercises.
+ */
 @Component
 public class ListExercisesUseCase implements ListExercises {
-
   private final ExerciseRepository exerciseRepositoryPort;
   private final ExercisePresenter presenter;
 
-  public ListExercisesUseCase(ExerciseRepository exerciseRepositoryPort, ExercisePresenter presenter) {
-    this.exerciseRepositoryPort = exerciseRepositoryPort;
+  /**
+   * Constructor for this use case.
+   *
+   * @param repository the exercise repository
+   * @param presenter  the exercise presenter
+   */
+  public ListExercisesUseCase(
+      ExerciseRepository repository,
+      ExercisePresenter presenter) {
+    this.exerciseRepositoryPort = repository;
     this.presenter = presenter;
   }
 
   @Override
   public Iterable<ExerciseResponse> listExercises() {
-    return StreamSupport.stream(exerciseRepositoryPort.getExercises().spliterator(), false)
-        .map(presenter::prepareResponse).toList();
+    return StreamSupport
+        .stream(exerciseRepositoryPort.getExercises().spliterator(), false)
+        .map(presenter::prepareResponse)
+        .toList();
   }
 
 }
