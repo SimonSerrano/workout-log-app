8ae20428ffb1998549b1037efe5681209cc70c06
Simon Serrano <simon.serrano@busit.com>
2025-02-03 17:11:16 +0100

    refactor(backend): added missing java doc

--- backend/src/main/java/com/marmouset/workout/app/port/in/workout/UpdateWorkoutLogCommand.java
+++ backend/src/main/java/com/marmouset/workout/app/port/in/workout/UpdateWorkoutLogCommand.java
@@ -2,5 +2,11 @@ package com.marmouset.workout.app.port.in.workout;
 
 import java.util.UUID;
 
+/**
+ * Command to update a workout log.
+ *
+ * @param id   the id of the workout
+ * @param name the name to update
+ */
 public record UpdateWorkoutLogCommand(UUID id, String name) {
 }
