a35252a3fb111f6c458699a7359bfb7216219ce8
Simon Serrano <simon.serrano@busit.com>
2025-02-03 15:25:29 +0100

    refactor(backend): added missing java doc

--- backend/src/main/java/com/marmouset/workout/app/port/out/exercise/CreateExerciseRepoRequest.java
+++ backend/src/main/java/com/marmouset/workout/app/port/out/exercise/CreateExerciseRepoRequest.java
@@ -1,5 +1,10 @@
 package com.marmouset.workout.app.port.out.exercise;
 
+/**
+ * Record to create an exercise in a repo.
+ *
+ * @param name the name of the exercise
+ */
 public record CreateExerciseRepoRequest(String name) {
 
 }
--- backend/src/main/java/com/marmouset/workout/app/port/out/workout/CreateWorkoutLogRepoRequest.java
+++ backend/src/main/java/com/marmouset/workout/app/port/out/workout/CreateWorkoutLogRepoRequest.java
@@ -1,5 +1,10 @@
 package com.marmouset.workout.app.port.out.workout;
 
+/**
+ * Record to create a workout log in a repository.
+ *
+ * @param name the name of the workout
+ */
 public record CreateWorkoutLogRepoRequest(String name) {
 
 }
