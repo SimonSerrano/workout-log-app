dba2cf373a9984290186bc8b2690b3eb0eca067f
Simon Serrano <simon.serrano@busit.com>
2025-02-03 15:09:06 +0100

    test(backend): added tests for exercise mapper

---
+++ backend/src/test/java/com/marmouset/workout/external/database/exercise/ExerciseMapperTest.java
@@ -0,0 +1,35 @@
+package com.marmouset.workout.external.database.exercise;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.marmouset.workout.app.domain.exercise.ExerciseFactory;
+import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseMapperTest {
+
+  @Autowired
+  private ExerciseFactory factory;
+  private ExerciseMapper mapper;
+
+
+  @BeforeEach
+  void setUp() {
+    mapper = new ExerciseMapper(factory);
+  }
+
+  @Test
+  void shouldMapToExercise() {
+    var entity = new ExerciseEntity();
+    entity.setId(UUID.randomUUID());
+    entity.setName("Pull up");
+
+    var expected = factory.create(entity.getId(), entity.getName());
+
+    assertEquals(mapper.toExercise(entity), expected);
+  }
+}
