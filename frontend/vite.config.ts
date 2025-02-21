26974fe43b563ca88976983c3590b9bde27d90d7
Simon Serrano <simon.serrano@busit.com>
2025-02-21 18:32:19 +0100

    chore(frontend): redirect api call to api container

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,15 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  server : {
+    proxy : {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
   plugins: [
     TanStackRouterVite(),
     react({
