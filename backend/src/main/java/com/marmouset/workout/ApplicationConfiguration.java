088c7e220d5f0894b1974fcb4b490a9908c0154f
Simon Serrano <simon.serrano@busit.com>
2025-02-21 17:58:56 +0100

    fix(backend): allow localhost on port 3000 now

--- backend/src/main/java/com/marmouset/workout/ApplicationConfiguration.java
+++ backend/src/main/java/com/marmouset/workout/ApplicationConfiguration.java
@@ -4,13 +4,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * Configuration for the application.
+ */
 @Configuration
 public class ApplicationConfiguration implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins("http://localhost:5173")
+        .allowedOrigins("http://localhost:3000")
         .allowedMethods("GET", "POST", "PATCH", "DELETE")
         .allowedHeaders("*")
         .allowCredentials(true);
