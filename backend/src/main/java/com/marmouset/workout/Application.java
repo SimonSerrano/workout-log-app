cb03ff72919ca60fd8243c39686297bc36c8968f
Simon Serrano <simon.serrano@busit.com>
2025-02-21 17:59:07 +0100

    refactor(backend): added missing java doc

--- backend/src/main/java/com/marmouset/workout/Application.java
+++ backend/src/main/java/com/marmouset/workout/Application.java
@@ -3,10 +3,19 @@ package com.marmouset.workout;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Application starter.
+ */
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+  /**
+   * Starts the application.
+   *
+   * @param args the args
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
 }
