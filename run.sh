233d63fa6e9a032cecc38d2dfa3b2f70f030d3c6
SimonSerrano <simon.serrano@hotmail.fr>
2025-03-03 10:51:22 +0100

    build: run script starts docker with build mode

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-docker-compose --env-file .env up db backend --watch &
+docker-compose --env-file .env up db backend --watch --build &
 cd frontend && npm run dev -- --port 3000 &
 
 wait
